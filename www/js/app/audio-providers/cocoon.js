cded8b6e728d89b578e4a66ce1b287ca4f3e34f7
Continuities <michael@doublespeakgames.com>
2015-10-31 11:22:21 -0400

    beginning cocoon audio

---
+++ www/js/app/audio-providers/cocoon.js
@@ -0,0 +1,8 @@
+/**
+ *  Cocoon Audio Provider
+ *  interface for playing audio using
+ *  Cocoon-compatible HTML audio
+ *  (c) doublespeak games 2015  
+ **/
+define(['app/promise'], function(Promise) {
+});
